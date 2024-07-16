0a568f7b7fa6c420864a659178ba60cdbbb8f919
marianodilup <mangypul@gmail.com>
2024-07-16 15:03:32 +0200

    Header branch merged

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,25 @@
-import { Routes } from "react-router-dom"
-import { ApiContext, useApiContext, } from "./Context/ApiContext"
-import CreatePost from "./CreatePost/CreatePost"
-
+import { Route, Routes } from "react-router-dom";
+import { ApiContext, useApiContext } from "./Context/ApiContext";
+import CreatePost from "./CreatePost/CreatePost";
+import NotFound from "./NotFound/NotFound";
+import Header from "./Header/Header";
+import Login from "./Login/Login";
 
 function App() {
-  const apiContext = useApiContext()
+  const apiContext = useApiContext();
 
   return (
     <>
-    <CreatePost/>
-< ApiContext.Provider value={apiContext}>
-<Routes>
-
-
-</Routes>
-</ApiContext.Provider>
-</>
-
-  )
+      <Header />
+      <ApiContext.Provider value={apiContext}>
+        <Routes>
+        <Route path="/" element={<Login/>} />
+          <Route path="/CreatePost" element={<CreatePost />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </ApiContext.Provider>
+    </>
+  );
 }
 
-export default App
+export default App;
