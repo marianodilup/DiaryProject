d7016280331bc6fb28120d87435ef7a2da79ef85
mateucarmen <carmen.mateu@bootcamp-upgrade.com>
2024-07-16 14:59:24 +0200

    select context imported to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,25 @@ import { Routes } from "react-router-dom"
 import { ApiContext, useApiContext, } from "./Context/ApiContext"
 import CreatePost from "./CreatePost/CreatePost"
 import Select from "./Select/Select"
+import { SelectContext, useSelectContext } from "./Context/SelectContext";
 
 
 function App() {
   const apiContext = useApiContext();
+  const selectContext = useSelectContext();
 
   return (
     <>
     
 < ApiContext.Provider value={apiContext}>
+  <SelectContext.Provider value={selectContext}>
 <CreatePost/>
   <Select />
 <Routes>
 
 
 </Routes>
+</SelectContext.Provider>
 </ApiContext.Provider>
 </>
 
