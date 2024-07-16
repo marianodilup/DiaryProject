17ba80848c3ac207049988dc0bc75343a7cfef49
marianodilup <mangypul@gmail.com>
2024-07-16 15:18:53 +0200

    Header merge and changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,9 @@
-<<<<<<< HEAD
-import { Route, Routes } from "react-router-dom";
-import { ApiContext, useApiContext } from "./Context/ApiContext";
-import CreatePost from "./CreatePost/CreatePost";
-import NotFound from "./NotFound/NotFound";
-import Header from "./Header/Header";
-import Login from "./Login/Login";
-
-function App() {
-  const apiContext = useApiContext();
-
-  return (
-    <>
-      <Header />
-      <ApiContext.Provider value={apiContext}>
-        <Routes>
-        <Route path="/" element={<Login/>} />
-          <Route path="/CreatePost" element={<CreatePost />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </ApiContext.Provider>
-    </>
-  );
-=======
 import { Routes } from "react-router-dom"
 import { ApiContext, useApiContext, } from "./Context/ApiContext"
 import CreatePost from "./CreatePost/CreatePost"
 import Select from "./Select/Select"
 import { SelectContext, useSelectContext } from "./Context/SelectContext";
+import 
 
 
 function App() {
@@ -49,7 +26,6 @@ function App() {
 </>
 
   )
->>>>>>> d7016280331bc6fb28120d87435ef7a2da79ef85
 }
 
 export default App;
