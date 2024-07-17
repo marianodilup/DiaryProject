86bd86a757c9cdc5e3631d5b3bbf5bb904e30ea4
Carlosgrpl <cgonzalez.38@alumni.unav.es>
2024-07-17 20:43:12 +0200

    Login comp change before merging

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+//import { userAPIjsx } from "../services/userapi.jsx";
 
-function Login() {
-  return <div>Login</div>;
+function Login({ setUserData }) {
+  const [user, setUser] = useState({ username: "", password: "" });
+
+  const handleInput = (ev) => {
+    const { id, value } = ev.target;
+    setUser((prevUser) => ({
+      ...prevUser,
+      [id]: value,
+    }));
+  };
+
+  const handleForm = async (ev) => {
+    ev.preventDefault();
+    try {
+      const data = await userAPIjsx(user);
+      localStorage.setItem("user", JSON.stringify(data));
+      setUserData(data);
+    } catch (error) {
+      console.error("Login failed", error);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleForm}>
+        <label htmlFor="username">Nombre de usuario</label>
+        <input
+          type="text"
+          id="username"
+          value={user.username}
+          onChange={handleInput}
+        />
+        <label htmlFor="password">Contraseña</label>
+        <input
+          type="password"
+          id="password"
+          value={user.password}
+          onChange={handleInput}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
