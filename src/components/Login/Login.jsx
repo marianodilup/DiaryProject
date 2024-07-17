cca6bdfb4c11e7ade54b392aa50d24ac18e2c299
Carlosgrpl <cgonzalez.38@alumni.unav.es>
2024-07-17 20:08:03 +0200

    LogIn Component done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import { userAPIjsx } from "../services/userapi.jsx";
 
-function Login() {
+const INITIAL_STATE = {
+  username: "",
+  password: "",
+};
+
+function Login({ setUserData }) {
+  const [user, setUser] = useState(INITIAL_STATE);
+
+  const handleInput = (ev) => {
+    setUser({ ...user, [ev.target.id]: ev.target.value });
+  };
+
+  const handleForm = (ev) => {
+    ev.preventDefault();
+    console.log(user);
+    userAPIjsx(user).then((data) => {
+      localStorage.setItem("user", JSON.stringify(data));
+      setUserData(data);
+    });
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div>
+      <form onChange={handleInput} onSubmit={handleForm}>
+        <label htmlFor="username">Nombre de usuario</label>
+        <input type="text" name="username" id="username" />
+        <label htmlFor="password">Contraseña</label>
+        <input type="password" name="password" id="password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
