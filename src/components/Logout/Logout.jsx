d32de7cf05e59d948fd152330e38cee7babf3220
Carlosgrpl <cgonzalez.38@alumni.unav.es>
2024-07-23 22:49:01 +0200

    Added reactive scss to LogOut and CreateEntry

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,43 +1,53 @@
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
-import Header from '../Header/Header';
-import { getDataUserForAuth } from '../Services/UserApi';
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Header from "../Header/Header";
+import { getDataUserForAuth } from "../Services/UserApi";
 
+function Logout({ userData, setUserData, authData, setAuthData }) {
+  const navigate = useNavigate();
 
-function Logout({userData, setUserData, authData, setAuthData}) {
-    const navigate = useNavigate();
-
-    useEffect(() => {
-      if (authData) {
+  useEffect(() => {
+    if (authData) {
       getDataUserForAuth(userData.token).then((info) => {
         console.log(info);
         setAuthData(info);
-      })
-    }}, []);
-  
-    const handleClickYes = () => {
-      setUserData(null);
-      localStorage.removeItem("user");
-      navigate("/");
-    };
-  
-    const handleClickNo = () => {
-      navigate("/ListPost")
-    };
-  
-    return (
-      <>
+      });
+    }
+  }, []);
+
+  const handleClickYes = () => {
+    setUserData(null);
+    localStorage.removeItem("user");
+    navigate("/");
+  };
+
+  const handleClickNo = () => {
+    navigate("/ListPost");
+  };
+
+  return (
+    <>
       <Header />
       <h2 className="logout-title">Cerrar sesión</h2>
       <div>
-          <p className="logout-msg">¿Estás seguro de que quieres cerrar sesión?</p>
-          <ul className="logout-options">
-              <li className="logout-yes-li"><button className="logout-btn" onClick={handleClickYes}>Sí</button></li>
-              <li className="logout-no-li"><button className="logout-btn" onClick={handleClickNo}>No</button></li>
-          </ul>
+        <p className="logout-msg">
+          ¿Estás seguro de que quieres cerrar sesión?
+        </p>
+        <ul className="logout-options">
+          <li className="logout-yes-li">
+            <button className="logout-btn" onClick={handleClickYes}>
+              Sí
+            </button>
+          </li>
+          <li className="logout-no-li">
+            <button className="logout-btn" onClick={handleClickNo}>
+              No
+            </button>
+          </li>
+        </ul>
       </div>
-      </>
-    )
+    </>
+  );
 }
 
-export default Logout
+export default Logout;
