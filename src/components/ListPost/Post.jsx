e86a112f804f4550e51105eb9d028fa0ac24b62f
mateucarmen <carmen.mateu@bootcamp-upgrade.com>
2024-07-24 19:06:44 +0200

    scss responsive for listpost done

--- src/components/ListPost/Post.jsx
+++ src/components/ListPost/Post.jsx
@@ -26,15 +26,18 @@ function Post({ post }) {
   return (
     <article className="diary-post">
       <Link className="link-post" to={`/detail/${post.id}`}>
-        <h4>{post.date}</h4>
-        <h3>{post.title}</h3>
-        <img
-          className="img-post"
-          src={post.img}
-          alt=""
-          width={300}
-          height={300}
-        />
+        <h4 className="post-date">{post.date}</h4>
+        <h3 className="post-title">{post.title}</h3>
+        <div className="img-desc-container">
+          <figure>
+          <img
+            className="img-post"
+            src={post.img}
+            alt={post.title}
+          />
+          </figure>
+          <p>{post.post.substring(0, 150)}...</p>
+        </div>
       </Link>
       <div className="btns-container">
         <button className="button-post" onClick={handleEdit} id={post.id}>
